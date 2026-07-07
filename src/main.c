8410630b5a7c41a0c89094ebbdfc6e9a9bac6f1e
Adam <ad1991kole@gmail.com>
2026-07-07 15:05:14 +0200

    created tui.c/h

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "network.h"
+#include "tui.h"
 
 int main(int argc, char *argv[]){
 	net_init();
+	outline_window();
 	printf("NetDet\n");
 	return 0;
 }
---
+++ src/tui.c
@@ -0,0 +1,8 @@
+#include <ncurses.h>
+#include <locale.h>
+#include <stdio.h>
+#include "tui.h"
+
+void outline_window(void){
+	printf("outline window\n");
+}
