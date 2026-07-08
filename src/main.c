c00242dcdadc2426e1b041f3281c02529b701573
Adam <ad1991kole@gmail.com>
2026-07-08 14:24:43 +0200

    changed how the main loop should end

--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
+#include <signal.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <ncurses.h>
 #include "network.h"
 #include "tui.h"
 
+volatile sig_atomic_t end_program = 0;
+
+void SIGINT_handler(int arg){
+	(void)arg;
+	end_program = 1;
+}
+
 int main(int argc, char *argv[]){
+
+	struct sigaction sa;
+	sa.sa_handler = SIGINT_handler;
+	sigfillset(&sa.sa_mask);
+	sigaction(SIGINT, &sa, NULL);
+
 	ncurses_init();
 
 	int input = 0;
@@ -20,7 +34,7 @@ int main(int argc, char *argv[]){
 		draw_window_frame(main_window, window_width, window_height);
 		wrefresh(main_window);
 		input = wgetch(main_window);
-	} while(input != 'q');
+	} while(!end_program);
 
 	delwin(main_window);
 	endwin();
