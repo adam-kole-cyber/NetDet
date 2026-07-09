f620363808116b34ed8c59f6b3655a8b52030f7f
Adam <ad1991kole@gmail.com>
2026-07-09 06:45:15 +0200

    created mvwprintIPw function

--- include/tui.h
+++ include/tui.h
@@ -16,5 +16,6 @@ void ncurses_init(void);
 void draw_window_frame(window_data *window_data, const char *title);
 void input_handler(window_data *window_data, int input);
 void resize_handler(window_data *window_data);
+void mvwprintIPw(window_data *window_data, int y, int x, const unsigned char *ip);
 
 #endif
