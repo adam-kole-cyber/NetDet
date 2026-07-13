0c3a9f00681ee14b2a7a40aeec11d163ec23ca7a
Adam <ad1991kole@gmail.com>
2026-07-13 22:40:46 +0200

    check if recived frame is arp frame

--- include/network.h
+++ include/network.h
@@ -1,7 +1,11 @@
 #ifndef NETWORK_H
 #define NETWORK_H
+
 #include <pthread.h>
 
+#define ETH_MAC_ADDRS_LEN 12	  // dst mac (6B) + src mac (6B)
+#define ETH_QinQ_TAG_LEN 4		  // in bytes
+#define ETH_QinQ_DOT1Q_TAGS_LEN 8 // in bytes
 extern pthread_t main_thread_id;
 
 struct network_thread_args {
--- src/network.c
+++ src/network.c
@@ -60,16 +60,31 @@ static void network_init(int *socket_fd, struct network_thread_args *args) {
 
 void *network_routine(void *args) {
 	int socket_fd;
-	char raw_frame_data[2048]; // expect a standard-length frame (as defined by IEEE 802.3), but I'm still leaving some room
-	ssize_t received_length = 0;
+	unsigned char raw_frame_data[2048]; // expect a standard-length frame (as defined by IEEE 802.3), but I'm still leaving some room
+	unsigned char processed_frame[2048];
+	ssize_t frame_length = 0;
 	network_init(&socket_fd, (struct network_thread_args *)args);
 
 	while (!end_listen_loop) {
-		received_length = recvfrom(socket_fd, raw_frame_data, sizeof(raw_frame_data), 0, NULL, NULL);
-		if (received_length < 0) {
+		frame_length = recvfrom(socket_fd, raw_frame_data, sizeof(raw_frame_data), 0, NULL, NULL);
+		if (frame_length < 0) {
 			if (errno == EAGAIN || errno == EWOULDBLOCK)
 				continue;
 			break;
+		}
+
+		if (raw_frame_data[12] == 0x08 && raw_frame_data[13] == 0x06) { // EtherType - 0x0806 -> ethernet frame
+			memcpy(processed_frame, raw_frame_data, ETH_MAC_ADDRS_LEN);
+			memset(&processed_frame[12], 0, ETH_QinQ_DOT1Q_TAGS_LEN);
+			memcpy(&processed_frame[20], &raw_frame_data[12], frame_length - ETH_MAC_ADDRS_LEN);
+			frame_length += 8;
+		} else if (raw_frame_data[12] == 0x81 && raw_frame_data[13] == 0x00) { // TPID - 0x8100 -> 802.1Q frame
+			memcpy(processed_frame, raw_frame_data, ETH_MAC_ADDRS_LEN);
+			memset(&processed_frame[12], 0, ETH_QinQ_TAG_LEN);
+			memcpy(&processed_frame[16], &raw_frame_data[12], frame_length - ETH_MAC_ADDRS_LEN);
+			frame_length += 4;
+		} else if (!(raw_frame_data[12] == 0x88 && raw_frame_data[13] == 0xa8)) { // TPID - 0x88a8 -> 802.1ad frame
+			continue;
 		}
 	}
 
