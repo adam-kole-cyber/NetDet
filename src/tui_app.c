929420f3429c0ad40063235aac28bb61870adf2f
Adam <ad1991kole@gmail.com>
2026-08-06 14:56:03 +0200

    realized that the condition could be simplified

--- src/tui_app.c
+++ src/tui_app.c
@@ -56,7 +56,7 @@ static void print_ip(WINDOW *window, const uint8_t *ip) {
 }
 
 static void print_qinq(WINDOW *window, const uint32_t *qinq_tag, bool highlight_line) {
-	if (((*qinq_tag & 0xfff) != 0) || ((*qinq_tag & 0xf000) != 0)) {
+	if ((*qinq_tag & 0xffff) != 0) {
 		char qinq_string[5];
 
 		snprintf(qinq_string, sizeof(qinq_string), "%u", (*qinq_tag & 0xfff));
@@ -81,7 +81,7 @@ static void print_qinq(WINDOW *window, const uint32_t *qinq_tag, bool highlight_
 }
 
 static void print_dot1q(WINDOW *window, const uint32_t *dot1q_tag, bool highlight_line) {
-	if (((*dot1q_tag & 0xfff) != 0) || ((*dot1q_tag & 0xf000) != 0)) {
+	if ((*dot1q_tag & 0xffff) != 0) {
 		char dot1q_string[5];
 
 		snprintf(dot1q_string, sizeof(dot1q_string), "%u", (*dot1q_tag & 0xfff));
